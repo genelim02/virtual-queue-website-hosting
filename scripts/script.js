af144de6140a2c3d2703e8c97d8a5c464b5ad42d
Wei Lin <weilinang.awl@gmail.com>
2021-02-01 22:33:55 +0800

    trying graph

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// import { GoogleCharts } from require('google-charts')
+// Vue.prototype.$GoogleCharts = GoogleCharts;
 let NumOfTrackers = 0;
 // let addbutton = document.getElementById('addbutton');
 //need a i=0 or smt then can + - the div when delete?
@@ -159,6 +161,31 @@ function showInactive(id){  //now working
     }
   }
 
+}
+window.onload = function(){
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+  }
+
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Year', 'Sales', 'Expenses'],
+    ['2004',  1000,      400],
+    ['2005',  1170,      460],
+    ['2006',  660,       1120],
+    ['2007',  1030,      540]
+  ]);
+
+  var options = {
+    title: 'Company Performance',
+    curveType: 'function',
+    legend: { position: 'bottom' }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+  chart.draw(data, options);
 }
 
 // })
